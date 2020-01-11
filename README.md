f6ebe3019a8de6bc0f222e92172ef2c1ae1692c1
kirill <kosaev-kira@mail.ru>
2020-01-11 12:50:20 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spaceSimulator
+
+[Creating Your Own Gravity and Space Simulator](https://css-tricks.com/creating-your-own-gravity-and-space-simulator/).
+
+```
+npm init
+npm install
+npm run dev
+
+http://localhost:8096/
+```
+
+
--- src/modules/helpers/getSettings.ts
+++ src/modules/helpers/getSettings.ts
@@ -1,53 +1,53 @@
 interface ISetting {
-    /**
+	/**
      * Константа масштаба
      *
      * @var number
      * */
-    scale: number
+	scale: number
 
-    /**
+	/**
      * Радиус тел
      *
      * @var number
      * */
-    radius: number
+	radius: number
 
-    /**
+	/**
      * Длина следа движения — количество предыдущих позиций, которые включает траектория
      *
      * @var number
      * */
-    trailLength: number
+	trailLength: number
 
-    /**
+	/**
      * Гравитационная постоянная
      *
      * @var number
      * */
-    g: number
+	g: number
 
-    /**
+	/**
      * Шаг времени - 0,008 года равно 2,92 дня
      *
      * @var number
      * */
-    dt: number
+	dt: number
 
-    /**
+	/**
      * Величина размягчения
      *  (для рассчета гравитационной силы)
      *
      * @var number
      * */
-    softeningConstant: number
+	softeningConstant: number
 }
 
 export default {
-    scale: 150,
-    radius: 5,
-    trailLength: 35,
-    g: 39.5,
-    dt: 0.008,
-    softeningConstant: 0.15
+	scale: 150,
+	radius: 5,
+	trailLength: 35,
+	g: 39.5,
+	dt: 0.008,
+	softeningConstant: 0.15,
 } as ISetting
